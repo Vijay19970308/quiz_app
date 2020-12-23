706c9edec0de7ae8fff023cdc0d6c7d8e76eb4df
Vijay19970308 <vijaydncbwc007@gmail.com>
2020-12-24 00:03:48 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Header from './Header';
-import Start from './Start';
-import Footer from './Footer';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-        <Header/>
-        <Start/>
-        <Footer/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/Database/Data.js
+++
@@ -1,7 +0,0 @@
-const Data =[{
-    username:"Vijay",
-    password:"1234",
-    phone:9876543210
-}]
-
-exports.Data = Data; 
